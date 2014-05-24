f004e4f058fac9890c55852d21327a300e8ebc72
Ron Smith <spinlock663@gmail.com>
2014-05-24 12:00:34 -0700

    changed to vector

--- PSF1/PSF1/utils.cpp
+++ PSF1/PSF1/utils.cpp
@@ -1,9 +1,13 @@
 //fucntions for computing image statistics in various formats.
 #include <iostream>
+#include <algorithm>
+#include <functional>
+#include <vector>
 #include "fftw3.h"
 #include "fitsio.h"
 #include "utils.h"
 
+using namespace std;
 
 float mean_float(float *data,int length)
 {
@@ -95,12 +99,8 @@ float background_v_float(float *data, int length)
 	float sigma=1;
 	float sigma_old=0;
 	float mean, median;
-	int b_length, b_trace;
-	float *b_set;
-	float *newb_set;
+	vector <float> b_set(data,data+length);
 	int i, j;
-	b_set = new float[length];
-	b_length = length;
 	bool sk_median_found = false;
 	for (i = 0; i < length; i++)
 	{
@@ -110,36 +110,7 @@ float background_v_float(float *data, int length)
 	sigma_old = sigma_float(data, mean, length);
 	while (!sk_median_found)
 	{
-		sk_median_found = true;
-		b_trace = b_length;
-		median = median_float(b_set, b_length);
-		mean = mean_float(b_set, b_length);
-		
-		
-			
-		for (i = 0; i < b_length; i++)
-		{
-
-			if (b_set[i]>(median + 3 * sigma))
-			{
-				sk_median_found = false;
-				b_trace--;
-			}
-		}
-		newb_set = new float[b_trace];
-		j = 0;
-		for (i = 0; i < b_length;i++)
-		{
-			if (b_set[i]>(median + 3 * sigma))
-			{
-				continue;
-			}
-			newb_set[j] = b_set[i];
-			j++;
-		}
-		delete b_set;
-		b_set = newb_set;
-		b_length = b_trace;
+		mean = mean_float(b_set.data(), b_set.size());
 
 	}
 	if (((sigma - sigma_old) / sigma) < 0.2)
