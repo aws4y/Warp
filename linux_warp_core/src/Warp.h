153e0349cdbbe99c2b6659c4a91c26f09f3fe1c2
Ron Smith <spinlock663@gmail.com>
2014-10-03 13:49:43 -0700

    Added some more functions
    Leaving work soon.

--- linux_warp_core/src/Warp.h
+++ linux_warp_core/src/Warp.h
@@ -8,7 +8,10 @@
 void warp_plugin_load(char *plug_file_name);
 template <class T> WarpImage<T> *sub_add(WarpImage<T>* im1, WarpImage<T> *im2, int length, int width, int x, int y); //adding subsets of images
 template <class T> WarpImage<T> *sub_subtract(WarpImage<T>* im1, WarpImage<T> *im2, int length, int width, int x, int y); //adding ub images
-template <class T> void rotate(WarpImage<T> *im, float rotation); //rotate an image representation in memory
+template <class T> WarpImage<T> * rotate(WarpImage<T> *im, float r); //returns an image that is a rotation of r radians of im 
 template <class T> WarpImage<T> * create_stack(int size); // Construct an image stack area from heap memory
+void destroy_stack(); //unwind the stack of images
+template <class T> WarpImage<T>* translate(WarpImage<T> *im, int x, int y); //returns a translation of im by the vector <x,y>
+
 
 #endif
