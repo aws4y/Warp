1e5433c9ccfed49e6c5e3f2611509944abe6bd29
Ron Smith <spinlock663@gmail.com>
2015-04-10 18:28:40 -0700

    Need to solve normal equations

--- linear-least-squares/linear-least-squares/Matrix.h
+++ linear-least-squares/linear-least-squares/Matrix.h
@@ -14,10 +14,33 @@ public:
 	~Matrix();
 	void display();
 	void input();
-	Matrix operator=(Matrix);
-
+	Matrix * Trans();
+	Matrix & operator=(Matrix *);
+	Matrix & operator* (Matrix *);
+	Matrix * Mult(Matrix *lhs, Matrix *rhs);
+	void set_element(int, int, double);
+const double get_element(int, int);
+const int get_rows();
+const	int get_columns();
 };
 
+const int Matrix::get_rows()
+{
+	return rows;
+}
+const int Matrix::get_columns()
+{
+	return columns;
+}
+const double Matrix::get_element(int i, int j)
+{
+	return matrix[i][j];
+}
+
+void Matrix::set_element(int i, int j, double val)
+{
+	matrix[i][j] = val;
+}
 Matrix::Matrix()
 {
 	rows = 0;
@@ -41,9 +64,10 @@ void Matrix::display()
 {
 	for (int i = 0; i < rows; i++)
 	{
+		cout << "|\t";
 		for (int j = 0; j < columns; j++)
-			cout << matrix[i][j] << " ";
-		cout << endl;
+			cout << matrix[i][j] << "\t";
+		cout <<"|"<< endl;
 	}
 	cout << endl;
 }
@@ -63,8 +87,63 @@ void Matrix::input()
 }
 Matrix:: ~Matrix()
 {
+	for (int i = 0; i < rows; i++)
+		delete[] matrix[i];
+	delete [] matrix;
+}
+Matrix * Matrix::Trans()
+{
+	Matrix *transpose;
+	transpose = new Matrix(columns, rows);
+	for (int i = 0; i < rows; i++)
+		for (int j = 0; j < columns; j++)
+			transpose->set_element(j, i, matrix[i][j]);
+	return transpose;
+}
 
-	delete matrix;
+Matrix & Matrix::operator=(Matrix *orig)
+{
+	if (matrix != nullptr)
+	{
+		delete matrix;
+	}
+
+	rows = orig->get_rows();
+	columns=orig->get_columns();
+	for (int i = 0; i < rows; i++)
+		matrix[i] = new double[columns];
+	for (int i = 0; i < rows; i++)
+		for (int j = 0; j < columns; j++)
+			matrix[i][j] = orig->get_element(i,j);
+	return *this;
+}
+
+Matrix & Matrix::operator*(Matrix * rhs )
+{
+	double sum;
+	Matrix *result;
+	if (columns != rhs->get_rows())
+	{
+		rows = 0;
+		columns = 0;
+		matrix = nullptr;
+		return *this;
+	}
+	result = new Matrix(rows, rhs->get_columns());
+	for (int i = 0; i < rows - 1; ++i)
+	{
+		for (int j = 0; j < rhs->get_columns() - 1; ++j)
+		{
+			sum = 0;
+			for (int k = 0; k < rhs->get_rows() - 1; ++k)
+			{
+				sum+= matrix[i][k] * rhs->get_element(k,j);
+			}
+
+			result->set_element(i,j,sum);
+		}
+	}
+	return *result;
 }
 
 
--- linear-least-squares/linear-least-squares/main.cpp
+++ linear-least-squares/linear-least-squares/main.cpp
@@ -1,20 +1,61 @@
 #include "Matrix.h"
 
+Matrix * Mult(Matrix *lhs, Matrix *rhs)
+{
+	double sum;
+	Matrix *result;
+	if (lhs->get_columns() != rhs->get_rows())
+	{
+		result = new Matrix();
+		return result;
+	}
+	result = new Matrix(lhs->get_rows(), rhs->get_columns());
+	for (int i = 0; i < result->get_rows(); i++)
+	{
+		for (int j = 0; j < result->get_columns(); j++)
+		{
+			sum = 0;
+			for (int k = 0; k < lhs->get_columns(); k++)
+			{
+				sum += lhs->get_element(i, k)*rhs->get_element(k, j);
+			}
+			result->set_element(i, j, sum);
+		}
+	}
+	return result;
+}
 
 int main(void)
 {
 	int n, N;
-	Matrix *input;
-	Matrix *normal;
+	Matrix *X;
+	Matrix *XT;
+	Matrix *XTX;
+	Matrix *y;
+	Matrix *XTy;
 	cout << "Number of unknowns: ";
 	cin >> n;
 	cout << "Number of Equations:";
 	cin >> N;
-	input = new Matrix (N, n + 1);
-	input->input();
-	input->display();
-
-	delete input;
+	X = new Matrix (N, n);
+	y = new Matrix(N, 1);
+	cout<< "Input coeffecient matrix:" << endl;
+	X->input();
+	cout << "Input equvalent vector:" << endl;
+	y->input();
+	X->display();
+	XT = X->Trans();
+	cout << "X transpose X:" << endl;
+	XTX =Mult(XT, X);
+	XTX->display();
+	cout << "X transpose y:" << endl;
+	XTy = Mult(XT, y);	
+	XTy->display();
+	delete X;
+	delete XT;
+	delete XTX;
+	delete y;
+	delete XTy;
 	cin.get();
 	cin.get();
 	return 0;
